b3c0baec81df6d19b51fb1baf9ef7a12ff5f2d68
EmilyCook321 <emilyacook16@gmail.com>
2020-08-19 14:30:39 +0200

    Add react router

--- src/pages/AddTask.js
+++ src/pages/AddTask.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function AddTask() {
+  const [task, setTask] = useState("");
+  const [author, setAuthor] = useState("");
+  const [priority, setPriority] = useState("");
+
+  function handleTaskChange(event) {
+    setTask(event.target.value);
+  }
+
+  function handleAuthorChange(event) {
+    setAuthor(event.target.value);
+  }
+
+  function handlePriorityChange(event) {
+    setPriority(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert("Submitted + task + author + priority");
+  }
+
   return (
-      <div>
-          <Link to="/">Tasks</Link>
-      </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Task:
+          <input value={task} onChange={handleTaskChange} />
+        </label>
+        <label>
+          Author:
+          <input value={author} onChange={handleAuthorChange} />
+        </label>
+        <label>
+          Priority
+          <input value={priority} onChange={handlePriorityChange} />
+        </label>
+      </form>
+      <Link to="/">Tasks</Link>
+    </>
   );
 }
 
