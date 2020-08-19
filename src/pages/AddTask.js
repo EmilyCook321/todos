4ce2bb9cf84ee410ebc1f10f41d5d8f07c307ee9
EmilyCook321 <emilyacook16@gmail.com>
2020-08-19 11:26:40 +0200

    Add rouer and pages

---
+++ src/pages/AddTask.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function AddTask() {
+  return (
+      <div>
+          <Link to="/">Tasks</Link>
+      </div>
+  );
+}
+
+export default AddTask;
