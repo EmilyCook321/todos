be36fc7d6f5392bcf32aa124e97f0698a397fa96
EmilyCook321 <emilyacook16@gmail.com>
2020-08-20 19:51:53 +0200

    Add error hhandling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AddTask from "./pages/AddTask";
-import { getTodos } from "./api/getTodos";
 
 function App() {
-  const [todos, setTodos] = useState(null);
-
-  use;
   return (
-    // <div className="App">
-    //   {todos?.map((todo) => (
-    //     <div key={todo.id}>{todo.task}</div>
-    //   ))}
-    // </div>
     <Router>
       <Switch>
         <Route path="/add">
--- src/pages/AddTask.js
+++ src/pages/AddTask.js
@@ -2,47 +2,51 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { postTodo } from "../api/getTodos";
 
-
 function AddTask() {
   const [task, setTask] = useState("");
   const [author, setAuthor] = useState("");
   const [priority, setPriority] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   function handleTaskChange(event) {
     setTask(event.target.value);
   }
-
   function handleAuthorChange(event) {
     setAuthor(event.target.value);
   }
-
   function handlePriorityChange(event) {
     setPriority(event.target.value);
   }
 
-  //     alert("Submitted + task + author + priority");
-  //   }
-
   async function handleSubmit(event) {
     event.preventDefault();
+
+    setLoading(true);
+    setError(false);
     const todo = {
       task,
       author,
-      priority,
       createdAt: Date.now(),
     };
 
-    await postTodo(todo);
-    setTask("");
-    setAuthor("");
-    setPriority("");
+    try {
+      await postTodo(todo);
+      setTask("");
+      setAuthor("");
+    } catch (error) {
+      console.error(error);
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
     <>
       <form onSubmit={handleSubmit}>
         <label>
-          Task:
+          Title:
           <input value={task} onChange={handleTaskChange} />
         </label>
         <label>
@@ -50,13 +54,19 @@ function AddTask() {
           <input value={author} onChange={handleAuthorChange} />
         </label>
         <label>
-          Priority
+          Priority:
           <input value={priority} onChange={handlePriorityChange} />
         </label>
+
+        <input
+          type="submit"
+          value="Add task"
+          disabled={!task || !author || loading}
+        />
+        {error && <p>Oh no! Something went wrong! Please try again.</p>}
       </form>
       <Link to="/">Tasks</Link>
     </>
   );
 }
-
 export default AddTask;
