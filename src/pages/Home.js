aa0bfd7c7774a65fbbe446fc50a7ad7698a5ec26
EmilyCook321 <emilyacook16@gmail.com>
2020-08-21 11:48:17 +0200

    Add async custom hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { getTodos } from "../api/getTodos";
 import { Link } from "react-router-dom";
+import useAsync from "../hooks/useAsync";
 
 function Home() {
-  const [todos, setTodos] = useState(null);
-  const doFetch = async () => {
-    const todos = await getTodos();
-    setTodos(todos);
-  };
-
-  useEffect(() => {
-    doFetch();
-  }, []);
+  const { data: todos, loading, error } = useAsync(getTodos);
 
   return (
     <div>
       <Link to="/add">Add Task</Link>
+      {error && <div>ERROR!</div>}
+      {loading && <div>Loading...</div>}
       {todos?.map((todo) => (
-        <div key={todo.id}>
-          {todo.task}, {todo.author},{todo.priority}
-        </div>
+        <div key={todo.id}>{todo.title}</div>
       ))}
     </div>
   );
 }
-
 export default Home;
