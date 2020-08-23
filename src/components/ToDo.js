e79ef93af450ca194fb95821d8821bad6602decc
EmilyCook321 <emilyacook16@gmail.com>
2020-08-23 19:33:09 +0200

    Redesign Todo

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
+
+export default function Todo({ todo }) {
+  const [complete, setComplete] = useState(todo.complete);
+
+  function completeTodo() {
+    setComplete(true);
+  }
+
+  function resetTodo() {
+    setComplete(false);
+  }
+
+  return (
+    <Container key={todo.id} complete={complete}>
+      <h2>{todo.title}</h2>
+      <Button onClick={completeTodo}>Complete</Button>
+      <ResetButton onClick={resetTodo}>Reset</ResetButton>
+    </Container>
+  );
+}
 
 const Container = styled.div`
   background-color: ${(props) => (props.complete ? "#06D6A0" : "#EF476F")};
+  padding: 10px;
+  border-radius: 15px;
+  margin: 5px;
 `;
 
-export default function Todo({ todo }) {
-  return <Container key={todo.id}>{todo.title}</Container>;
-}
+// export default function Todo({ todo }) {
+//   return <Container key={todo.id}>{todo.title}</Container>;
+// }
 
 // export default function Todo({ todo }) {
 //     const [complete, setComplete] = useState(todo.complete);
